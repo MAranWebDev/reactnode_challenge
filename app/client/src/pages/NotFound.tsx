d4e1c03649d7f84306e2c3b627857f7373401963
MAranWebDev <mario.arancibia.jimenez@gmail.com>
2022-07-20 08:31:44 -0400

    blank variable refactor in Not Found page

--- app/client/src/pages/NotFound.tsx
+++ app/client/src/pages/NotFound.tsx
@@ -3,7 +3,7 @@ import { Body } from "../components/Layout/Body";
 
 const NotFound = () => {
   return (
-    <Body title="404" blank={true}>
+    <Body title="404" blank>
       <section className="m-auto text-center">
         <h1 className="display-2">404 Página no encontrada</h1>
         <Link to="/" className="btn btn-outline-dark btn-lg">
